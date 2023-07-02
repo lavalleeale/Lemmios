ac5aeaf063ca34022db73ad8faebbb6d3f0e335a
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2023-07-02 12:17:12 -0700

    Initial Commit

---
+++ Artemis/ArtemisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtemisApp.swift
+//  Artemis
+//
+//  Created by Alex lavallee on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ArtemisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
