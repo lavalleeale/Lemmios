d34b35590d0f9c42f6615c074b4780ba7a5ed533
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2023-08-03 23:04:18 -0700

    fix(Swiping): Always start swipe
    Closes #42

--- Lemmios/Views/SwipeActionsModifier.swift
+++ Lemmios/Views/SwipeActionsModifier.swift
@@ -72,7 +72,7 @@ struct SwiperContainer: ViewModifier {
             }
         }
         .contentShape(Rectangle())
-        .gesture(DragGesture(minimumDistance: 40, coordinateSpace: .local)
+        .highPriorityGesture(DragGesture(minimumDistance: 40, coordinateSpace: .local)
             .onChanged { value in
                 var totalSlide = value.translation.width
                 if totalSlide < 0, trailingOptions.isEmpty {
