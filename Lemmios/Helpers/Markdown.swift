a5d0b413d323c32cc9136163c9270a319e64834d
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2023-08-07 01:52:01 -0700

    fix(Markdown): Fix domain name regex

--- Lemmios/Helpers/Markdown.swift
+++ Lemmios/Helpers/Markdown.swift
@@ -178,7 +178,7 @@ struct MarkdownView: View {
 
 func processMarkdown(input: String, stripImages: Bool) -> String {
     var output = input.replacingOccurrences(of: "( |^)(/[cm]/|!)([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\\.[a-zA-Z]{2,})", with: "[$0](https://$4/c/$3)", options: .regularExpression)
-    output = output.replacingOccurrences(of: "( |^)(?:/u/|@)([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+.[a-zA-Z]{2,})", with: "[$0](https://$3/u/$2)", options: .regularExpression)
+    output = output.replacingOccurrences(of: "( |^)(?:/u/|@)([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\\.[a-zA-Z]{2,})", with: "[$0](https://$3/u/$2)", options: .regularExpression)
     if stripImages {
         output = output.replacingOccurrences(of: "![", with: "[").replacingOccurrences(of: "[]", with: "[image]")
     }
